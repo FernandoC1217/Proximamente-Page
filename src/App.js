3a84f4b76e451fd25c76ab3c41cb25254f911c04
Fernando Cutire UTP <fernando.cutire@utp.ac.pa>
2020-10-31 21:57:11 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Próximamente</h1>
+        <p>Actualmente estamos trabajando duro en esta página</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Notifícame cuandone esté lista
         </a>
       </header>
     </div>
